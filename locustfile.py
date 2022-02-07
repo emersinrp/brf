d46a488824dc8558087bba8786f279576156667d
Emerson Rodrigues <emersinrp@gmail.com>
2022-02-07 18:26:54 -0300

    Add tests on APi - Costumer Devolucoes

--- locustfile.py
+++ locustfile.py
@@ -3,21 +3,27 @@ from json import load, loads
 from random import choice
 
 
-class CargaAtendimentoStatusCliente(HttpUser):
+class CargaTestsApisTina(HttpUser):
 
     wait_time = between(1.0, 3.0)
 
     ENDPOINT_PRIFIX_ATENDIMENTO = "/api/StatusCliente"
+    ENDPOINT_PRIFIX_CUSTOMER_DEVOLUCOES = "/api/Devolucoes"
     COD_VENDEDOR = [
         "00511899",
         "00005095",
         "00015614"
     ]
+    COD_VENDEDOR_CUSTOMER = [
+        "00580849"
+    ]
     COD_CLIENTE = [
         "0001005443"
     ]
 
-    @task  # (2) definir o peso = priorizacao de execucao.
+# APIs Atendimento - Status Cliente
+
+#    @task  # (2) definir o peso = priorizacao de execucao.
     def busca_cli_des_bloq(self):
 
         consult_cliente_endpoint = f"{self.ENDPOINT_PRIFIX_ATENDIMENTO}/BuscaClientesBloqueadosDesbloqueados"
@@ -34,7 +40,7 @@ class CargaAtendimentoStatusCliente(HttpUser):
             resposta = loads(response.text or "null")
 
             try:
-                print(f"01 - Consulta cliente bloqueado e desbloqueado \n {resposta['meta']['count']}")
+                print(f"============= \n 01 - Consulta cliente bloqueado e desbloqueado \n {resposta['meta']['count']}")
                 if resposta["meta"]["count"] == 0:
                     response.failure(
                         "Count retornou = 0"
@@ -44,10 +50,8 @@ class CargaAtendimentoStatusCliente(HttpUser):
                 response.failure(
                     "Nao foi possivel acessar o valor meta.count"
                 )
-            #            if not response.status_code == 404:
-            #                response.failure(f"O status da resposta foi {response.status_code}, que e o diferente do esperado 404")
 
-    @task
+#    @task
     def busca_status_cliente(self):
 
         consult_client_endpoint = f"{self.ENDPOINT_PRIFIX_ATENDIMENTO}/BuscaStatusCliente"
@@ -61,7 +65,7 @@ class CargaAtendimentoStatusCliente(HttpUser):
             resposta = loads(response.text or "null")
 
             try:
-                print(f"02 - Consulta status cliente \n {resposta['success']} \n {len(resposta['data'])} \n Status Code: {response.status_code}")
+                print(f"============= \n 02 - Consulta status cliente \n {resposta['success']} \n {len(resposta['data'])} \n Status Code: {response.status_code}")
                 if resposta["success"] != True and len(resposta["data"]) != 21:
                     response.failure(f"Corpo de resposta diferente do esperado: {response.text}")
             except KeyError:
@@ -70,7 +74,7 @@ class CargaAtendimentoStatusCliente(HttpUser):
                     "Nao foi possivel acessar os valores success/data"
                 )
     
-    @task
+#    @task
     def obter_cliente_montante(self):
 
         consult_client_endpoint = f"{self.ENDPOINT_PRIFIX_ATENDIMENTO}/ObterClientesMontantes"
@@ -87,7 +91,7 @@ class CargaAtendimentoStatusCliente(HttpUser):
 
             try:
                 print(
-                    f"03 - Obter clientes montantes \n {resposta['success']} \n {len(resposta['data'])} \n Status Code: {response.status_code}")
+                    f"============= \n 03 - Obter clientes montantes \n {resposta['success']} \n {len(resposta['data'])} \n Status Code: {response.status_code}")
                 if resposta["success"] != True and len(resposta["data"]) != 24:
                     response.failure(
                         f"Corpo de resposta diferente do esperado: {response.text}")
@@ -98,10 +102,114 @@ class CargaAtendimentoStatusCliente(HttpUser):
                     "Nao foi possivel acessar os valores de data"
                 )
 
-        
+# APIs Customer - Devoluções
 
+    @task
+    def busca_devolucoes_categoria(self):
 
+        consult_client_endpoint = f"{self.ENDPOINT_PRIFIX_CUSTOMER_DEVOLUCOES}/BuscarDevolucoesPorCategoria"
+        body = {
+           "codVendedor": self.COD_VENDEDOR_CUSTOMER[0],
+           "codCliente": "",
+           "periodo": "m",
+           "categoriaGM": "LINGUICA FRESCA (DOMESTICA)"
+        }
+        with self.client.post(url=consult_client_endpoint,
+                              name="01 - Busca devoluções por categorias",
+                              catch_response=True, json=body) as response:
+            resposta = loads(response.text or "null")
 
+            try:
+                print(
+                    f"============= \n 01 - Busca devolucoes por categoria \n {resposta['success']} \n {len(resposta['data'])} \n Status Code: {response.status_code}")
+                if resposta["success"] != True and len(resposta["data"]) != 5:
+                    response.failure(
+                        f"Corpo de resposta diferente do esperado: {response.text}")
+            except KeyError:
+                print(
+                    f"01 - FALHA ao obter resultado de devolucoes por categoria \n {response.text} \n Status code: {response.status_code}")
+                response.failure(
+                    "Nao foi possivel acessar os valores de data"
+                )
 
-# codigos = {COD_VENDEDOR:[COD_CLIENTE]}
-# codigos[COD_VENDEDOR][0]
+    @task
+    def busca_devolucao_cliente(self):
+
+        consult_client_endpoint = f"{self.ENDPOINT_PRIFIX_CUSTOMER_DEVOLUCOES}/BuscarDevolucoesPorCliente"
+        body = {
+           "codVendedor": self.COD_VENDEDOR_CUSTOMER[0],
+           "codCliente": "0000091263",
+           "periodo": "m"
+        }
+        with self.client.post(url=consult_client_endpoint,
+                              name="02 - Busca devoluções por cliente",
+                              catch_response=True, json=body) as response:
+            resposta = loads(response.text or "null")
+
+            try:
+                print(
+                    f"============= \n 02 - Busca devolucoes por cliente \n {resposta['success']} \n {len(resposta['data'])} \n Status Code: {response.status_code}")
+                if resposta["success"] != True and len(resposta["data"]) != 7:
+                    response.failure(
+                        f"Corpo de resposta diferente do esperado: {response.text}")
+            except KeyError:
+                print(
+                    f"02 - FALHA ao obter resultado de devolucoes por cliente \n {response.text} \n Status code: {response.status_code}")
+                response.failure(
+                    "Nao foi possivel acessar os valores de data"
+                )
+
+    @task
+    def busca_devolucao_sku(self):
+
+        consult_client_endpoint = f"{self.ENDPOINT_PRIFIX_CUSTOMER_DEVOLUCOES}/BuscarDevolucoesPorSKU"
+        body = {
+           "codVendedor": self.COD_VENDEDOR_CUSTOMER[0],
+           "codCliente": "0000091263",
+           "periodo": "m",
+           "nome": "",
+           "categoriaGM": "LINGUICA FRESCA (DOMESTICA)" 
+        }
+        with self.client.post(url=consult_client_endpoint,
+                              name="03 - Busca devolucoes por SKU",
+                              catch_response=True, json=body) as response:
+            resposta = loads(response.text or "null")
+
+            try:
+                print(
+                    f"============= \n 03 - Busca devolucoes por SKU \n {resposta['success']} \n {len(resposta['data'])} \n Status Code: {response.status_code}")
+                if resposta["success"] != True and len(resposta["data"]) != 9:
+                    response.failure(
+                        f"Corpo de resposta diferente do esperado: {response.text}")
+            except KeyError:
+                print(
+                    f"03 - FALHA ao obter resultado de devolucoes por SKU \n {response.text} \n Status code: {response.status_code}")
+                response.failure(
+                    "Nao foi possivel acessar os valores de data"
+                )
+
+    @task
+    def soma_devolucoes(self):
+
+        consult_client_endpoint = f"{self.ENDPOINT_PRIFIX_CUSTOMER_DEVOLUCOES}/GetSomaDevolucoes"
+        body = {
+           "codVendedor": self.COD_VENDEDOR_CUSTOMER[0],
+           "codCliente": "0000091263" 
+        }
+        with self.client.post(url=consult_client_endpoint,
+                              name="04 - Busca soma devoluções",
+                              catch_response=True, json=body) as response:
+            resposta = loads(response.text or "null")
+
+            try:
+                print(
+                    f"============= \n 04 - Busca soma de devolucoes \n {resposta['success']} \n {len(resposta['data'])} \n Status Code: {response.status_code}")
+                if resposta["success"] != True and len(resposta["data"]) != 5 and len(resposta["grafico"]) > 10:
+                    response.failure(
+                        f"Corpo de resposta diferente do esperado: {response.text}")
+            except KeyError:
+                print(
+                    f"04 - FALHA ao obter resultado da soma de devolucoes \n {response.text} \n Status code: {response.status_code}")
+                response.failure(
+                    "Nao foi possivel acessar os valores de data ou grafico"
+                )
